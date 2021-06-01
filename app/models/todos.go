fee63c11fddd07972c58e5d069f85442048140b7
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-06-02 07:59:50 +0900

    feat: todo取得機能作成

--- app/models/todos.go
+++ app/models/todos.go
@@ -32,3 +32,24 @@ func (u *User) CreateTodo(content string) (err error) {
 
 	return err
 }
+
+func GetTodo(id int) (todo Todo, err error) {
+	// Connent to DB
+	Db, err = connectDB()
+	defer Db.Close()
+
+	cmd := `SELECT id, content, user_id, created_at, updated_at
+		FROM todos
+		WHERE id =$1`
+	todo = Todo{}
+
+	err = Db.QueryRow(cmd, id).Scan(
+		&todo.ID,
+		&todo.Content,
+		&todo.UserID,
+		&todo.CreatedAt,
+		&todo.UpdatedAt,
+	)
+
+	return todo, err
+}
