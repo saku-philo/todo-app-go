966e1d402f726de8f95106f62ce9cc0060adf776
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-06-16 23:10:10 +0900

    feat: create signup form

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, "yahoo", "layout", "top")
+	generateHTML(w, "yahoo", "layout", "public_navbar", "top")
 	// t, err := template.ParseFiles("app/views/templates/top.html")
 	// if err != nil {
 	// 	log.Fatalln(err)
