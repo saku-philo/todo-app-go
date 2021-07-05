9f12b98bf1ffc51f8a0ad6aeed21804dfad832c9
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-07-05 22:56:49 +0900

    feat: login状況による画面表示制御機能作成

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -5,10 +5,19 @@ import (
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, "yahoo", "layout", "public_navbar", "top")
-	// t, err := template.ParseFiles("app/views/templates/top.html")
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// t.Execute(w, "yahoo!")
+	_, err := session(w, r)
+	if err != nil {
+		generateHTML(w, "yahoo", "layout", "public_navbar", "top")
+	} else {
+		http.Redirect(w, r, "/todos", 302)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	_, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/", 302)
+	} else {
+		generateHTML(w, nil, "layout", "private_navbar", "index")
+	}
 }
